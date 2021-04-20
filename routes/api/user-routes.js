4f8a27aeeaab0d997b9f3a3b4453ab5a1e0b1575
Sydney Walcoff <sydney.walcoff@gmail.com>
2021-04-20 12:38:29 -0700

    users can login now

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -49,6 +49,28 @@ router.post('/', (req, res) => {
     });
 });
 
+router.post('/login', (req, res)  => {
+    User.findOne({
+        where: {
+            email: req.body.email
+        }
+    }).then(dbUserData => {
+        if(!dbUserData) {
+            res.status(400).json({ message: 'No user with that email address!'});
+            return;
+        }
+
+        // Verify user
+        const validPassword = dbUserData.checkPassword(req.body.password);
+
+        if(!validPassword) {
+            res.status(400).json({ message: 'Incorrect password!' });
+            return;
+        }
+        res.json({ user: dbUserData, message: 'You are now logged in!' });
+    });
+});
+
 // UPDATE api/users/1
 router.put('/:id', (req, res) => {
     User.update(req.body, {
