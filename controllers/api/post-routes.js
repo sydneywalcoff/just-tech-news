c17368dda072696179209e577225800635055c2a
Sydney Walcoff <sydney.walcoff@gmail.com>
2021-04-29 11:21:04 -0700

    upvotes functional

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -71,14 +71,16 @@ router.post('/', (req, res) => {
     });
 });
 
-// PUT /api/posts/upvote
+// add an upvote
 router.put('/upvote', (req, res) => {
-    Post.upvote(req.body, { Vote })
-    .then(updatedPostData => res.json(updatedPostData))
-    .catch(err => {
-        console.log(err);
-        res.status(400).json(err);
-    });
+    if(req.session) {
+        Post.upvote({ ... req.body, user_id: req.session.user_id }, { Vote, Comment, User })
+        .then(updatedVoteData => res.json(updatedVoteData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        })
+    }
 });
 
 router.put('/:id', (req, res) => {
