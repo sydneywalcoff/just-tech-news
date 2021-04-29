4b59c376e7af85977a4c9770dcfe30d2d0c7fdf5
Sydney Walcoff <sydney.walcoff@gmail.com>
2021-04-29 10:47:34 -0700

    single-page template correctly receiving data, but buttons dont work

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
 
+// homepage
 router.get('/', (req, res) => {
     console.log(req.session);
     Post.findAll({
@@ -37,6 +38,7 @@ router.get('/', (req, res) => {
     });
 });
 
+// login/signup page
 router.get('/login', (req, res) => {
     if(req.session.loggedIn) {
         res.redirect('/');
@@ -44,5 +46,49 @@ router.get('/login', (req, res) => {
     }
     res.render('login');
 });
+
+// get single post
+router.get('/post/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'post_url',
+            'title',
+            'created_at',
+            [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(dbPostData => {
+        if(!dbPostData) {
+            res.status(404).json({ message: 'No post found with this id' });
+            return;
+        }
+
+        const post = dbPostData.get({ plain: true });
+
+        res.render('single-post', { post });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+  });
 
 module.exports = router;
